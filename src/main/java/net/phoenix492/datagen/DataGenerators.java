cc16cfcbf92e572cfc8d7131c392d37fb1cdbafd
pieman492 <rauljrrauljr@gmail.com>
2025-09-06 07:13:46 -0500

    Datagen setup

---
+++ src/main/java/net/phoenix492/datagen/DataGenerators.java
@@ -0,0 +1,25 @@
+package net.phoenix492.datagen;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.PackOutput;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.data.event.GatherDataEvent;
+import net.phoenix492.simpledamagecaps.SimpleDamageCaps;
+
+import java.util.concurrent.CompletableFuture;
+
+@EventBusSubscriber(modid = SimpleDamageCaps.MODID)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        PackOutput packOutput = generator.getPackOutput();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+        CompletableFuture<HolderLookup.Provider> lookupProvider = event.getLookupProvider();
+
+        generator.addProvider(event.includeServer(), new ModDataMapProvider(packOutput, lookupProvider));
+    }
+}
