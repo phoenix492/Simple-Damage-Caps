c37759ed528b3081f33ae34753844377af5faa3f
pieman492 <rauljrrauljr@gmail.com>
2025-09-06 07:08:16 -0500

    Initial repo setup

---
+++ src/main/java/net/phoenix492/simpledamagecaps/Config.java
@@ -0,0 +1,9 @@
+package net.phoenix492.simpledamagecaps;
+
+import net.neoforged.neoforge.common.ModConfigSpec;
+
+public class Config {
+    private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
+
+    static final ModConfigSpec SPEC = BUILDER.build();
+}
